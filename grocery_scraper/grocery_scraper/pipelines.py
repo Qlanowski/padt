371d30211a652bda52bd60487281a8fafc22cd10
Michał Urawski <michurawski@gmail.com>
2020-11-19 17:00:07 +0100

    Ogarnalem popite

--- grocery_scraper/grocery_scraper/pipelines.py
+++ grocery_scraper/grocery_scraper/pipelines.py
@@ -15,10 +15,24 @@ def text_to_ean(ean):
     match = re.search(r'\d+', ean)
     return match.group(0) if match is not None else None
 
+def parse_nutrition(nutrition):
+    if nutrition is None:
+        return None
+    
+    res = {}
+
+    for key in nutrition:
+        new_key = key.strip()
+        new_key = re.sub(r'[*%]', '', new_key)
+        res[new_key] = nutrition[key]
+    return res
+
 class GroceryScraperPipeline(object):
     def process_item(self, item, spider):
         item['price'] = text_to_number(item['price'])
         item['ean'] = text_to_ean(item['ean'])
         item['price_before_discount'] = text_to_number(item['price_before_discount'])
         item['price_per_quantity'] = text_to_number(item['price_per_quantity'])
+        item['nutrition'] = parse_nutrition(item['nutrition'])
+
         return item
